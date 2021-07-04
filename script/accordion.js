96c5482b8437c46a61a47125a4c879683841b90c
YuriBorysiuk <65165493+YuriBorysiuk@users.noreply.github.com>
2021-07-04 17:34:43 +0300

    finish accordion

--- script/accordion.js
+++ script/accordion.js
@@ -1,16 +1,24 @@
 const featureLinkElems = document.querySelectorAll('.feature__link')
 const featureSubElems = document.querySelectorAll('.feature-sub')
 
-// for (let i = 0; i < featureLinkElems.length; i++) {
-//     featureLinkElems[i].addEventListener('click', () => {
-//         featureSubElems[i].classList.toggle('hidden')
-//         featureLinkElems[i].classList.toggle('feature__link_active')
-//     })
-// } 
 
 featureLinkElems.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        featureSubElems[index].classList.toggle('hidden')
-        btn.classList.toggle('feature__link_active')
-    })
+	btn.addEventListener('click', () => {
+		console.log(btn.classList.contains('feature__link_active'));
+		if (btn.classList.contains('feature__link_active')) {
+			btn.classList.remove('feature__link_active')
+			featureSubElems[index].classList.add('hidden')
+		} else {
+			featureSubElems.forEach((featureSubElem) => {
+				featureSubElem.classList.add('hidden')
+				console.log('featureSubElem: ', featureSubElem);
+			})
+			featureLinkElems.forEach((featureLinkElem) => {
+				featureLinkElem.classList.remove('feature__link_active')
+				
+			})
+			featureSubElems[index].classList.remove('hidden')
+			btn.classList.add('feature__link_active')
+		}
+	})
 })
