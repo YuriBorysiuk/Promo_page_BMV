ee81ba5f4e64f20bb5287207e6a041a31d669968
YuriBorysiuk <65165493+YuriBorysiuk@users.noreply.github.com>
2021-07-06 01:27:56 +0300

    implemented smooth scroll

---
+++ script/smoothScroll.js
@@ -0,0 +1,13 @@
+//Обращаемся ко всем ссылкам, начинающихся в href с #, кроме ссылок у которых только решетка
+const smothScrollElems = document.querySelectorAll ('a[href^="#"]:not(a[href="#"])')
+
+smothScrollElems.forEach(link => {
+	link.addEventListener('click', (event) => {
+		
+		event.preventDefault()
+		const id = link.getAttribute('href').substring(1)
+		document.getElementById(id).scrollIntoView({
+			behavior: 'smooth'
+		})
+	})
+})
